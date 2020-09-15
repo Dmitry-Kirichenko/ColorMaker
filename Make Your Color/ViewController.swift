a5ce853f1ee4fac18852e276c8fa4ecd9763c111
Dmitry Kyrychenko <kirichenkodmitryy@gmail.com>
2020-09-15 18:51:42 +0300

    5.The color of view changes,when you move the slider or enter value in text field

--- Make Your Color/ViewController.swift
+++ Make Your Color/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet var topLabel: UILabel!
     @IBOutlet var redLabel: UILabel!
@@ -28,7 +28,6 @@ class ViewController: UIViewController {
     
     @IBOutlet var finalColor: UIView!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -46,8 +45,26 @@ class ViewController: UIViewController {
         greenTextField.placeholder = "1.00"
         blueTextField.placeholder = "1.00"
         
+        //Create toolbar and "Done" button
+        let toolBar = UIToolbar()
+        
+        let doneButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.done, target: self, action: #selector(doneButtonTapped))
+        
+        let flexibleSpace = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: nil, action: nil)
+        
+        toolBar.sizeToFit()
+        toolBar.setItems([flexibleSpace, doneButton], animated: false)
+        
+        redTextField.inputAccessoryView = toolBar
+        greenTextField.inputAccessoryView = toolBar
+        blueTextField.inputAccessoryView = toolBar
+        
     }
 
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        doneButtonTapped()
+    }
+    
     @IBAction func slidersAction() {
         let redValue = CGFloat(redSlider.value)
         let greenValue = CGFloat(greenSlider.value)
@@ -58,10 +75,33 @@ class ViewController: UIViewController {
         redValueLabel.text = String(format: "%.2f", redValue)
         greenValueLabel.text = String(format: "%.2f", greenValue)
         blueValueLabel.text = String(format: "%.2f", blueValue)
+        
+        redTextField.text = redValueLabel.text
+        greenTextField.text = greenValueLabel.text
+        blueTextField.text = blueValueLabel.text
     }
     
+    
+    @objc func doneButtonTapped() {
+        
+        view.endEditing(true)
+        
+        guard let redValueText = redTextField.text, !redValueText.isEmpty else { return }
+        guard let greenValueText = greenTextField.text, !greenValueText.isEmpty else { return }
+        guard let blueValueText = blueTextField.text, !blueValueText.isEmpty else { return }
+        
+        if let redValue = Float(redValueText){
+            redSlider.value = redValue
+            slidersAction()
+        }
+        if let greenValue = Float(greenValueText){
+            greenSlider.value = greenValue
+            slidersAction()
+        }
+        if let blueValue = Float(blueValueText){
+            blueSlider.value = blueValue
+            slidersAction()
+        }
+    }
 }
  
-//extension ViewController, UITextFieldDelegate {
-//    
-//}
